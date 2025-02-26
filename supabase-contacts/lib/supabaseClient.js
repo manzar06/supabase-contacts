455adde211269986429903e4e1c0c6b5c48e130f
manzar06 <siddiquimanzar05@gmail.com>
2025-02-27 01:05:56 +0530

    Updated environment variables

--- supabase-contacts/lib/supabaseClient.js
+++ supabase-contacts/lib/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://lvagujspwtzzdhfsrzfy.supabase.co";
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2YWd1anNwd3R6emRoZnNyemZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1ODIzNDgsImV4cCI6MjA1NjE1ODM0OH0.Vg8_SJSVyQRoDQU6Jgm8IMKflyDRDmWNFwK9VMoWVOM";
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
